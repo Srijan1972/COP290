83e990cc29942add4fca4418bd3c28be8a21f6c7
NischayDiwan <cs5200433@iitd.ac.in>
2022-02-12 15:17:17 +0530

    my path added

--- Task1/Task1.2/matrix_openblas.cpp
+++ Task1/Task1.2/matrix_openblas.cpp
@@ -1,4 +1,4 @@
-#include<cblas.h>
+#include"x86_64-linux-gnu/openblas64-pthread/cblas.h"
 
 void fullyConnectedOpen(float* inp,float* weight,float* bias,int a,int b,int c){
     cblas_sgemm(CblasColMajor,CblasNoTrans,CblasNoTrans,a,c,b,1.0,inp,a,weight,b,1.0,bias,a);
