6f44f56f9516ddce47de129f8511a1134c1fb2f6
Srijan1972 <srijang@live.com>
2022-02-20 22:22:44 +0530

    Time measurements

--- Task1/Task1.2/matrix_openblas.cpp
+++ Task1/Task1.2/matrix_openblas.cpp
@@ -1,5 +1,50 @@
-#include"x86_64-linux-gnu/openblas64-pthread/cblas.h"
+#include<cblas.h>
+#include<bits/stdc++.h>
 
-void fullyConnectedOpen(float* inp,float* weight,float* bias,int a,int b,int c){
-    cblas_sgemm(CblasColMajor,CblasNoTrans,CblasNoTrans,a,c,b,1.0,inp,a,weight,b,1.0,bias,a);
+int main(int argc,char** argv){
+    std::string* args=new std::string[argc];
+    for(int i=0;i<argc;i++){
+        std::string temp(argv[i]);
+        args[i]=temp;
+    }
+    if(argc!=7){
+        std::cout<<"Invalid number of arguments"<<std::endl;
+        return 1;
+    }
+    if(args[1]!="fullyconnected"){
+        std::cout<<"Invalid operation to be performed"<<std::endl;
+        return 1;
+    }
+    std::ifstream inpin(args[2]);
+    std::ifstream weightin(args[3]);
+    std::ifstream biasin(args[4]);
+    std::ofstream resout(args[5]);
+    if(!inpin || !weightin || !biasin){
+        std::cout<<"File not found"<<std::endl;
+        return 1;
+    }
+    if(args[6]=="openblas"){
+        int b,a,c;
+        inpin>>b>>a;
+        float* inp=new float[a*b];
+        for(int i=0;i<a*b;i++) inpin>>inp[i];
+        weightin>>c>>b;
+        float* weight=new float[b*c];
+        for(int i=0;i<b*c;i++) weightin>>weight[i];
+        biasin>>c>>a;
+        float* bias=new float[c*a];
+        for(int i=0;i<c*a;i++) biasin>>bias[i];
+        auto start=std::chrono::high_resolution_clock::now();
+        cblas_sgemm(CblasColMajor,CblasNoTrans,CblasNoTrans,a,c,b,1.0,inp,a,weight,b,1.0,bias,a);
+        auto end=std::chrono::high_resolution_clock::now();
+        std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()<<std::endl;
+        resout<<c<<"\n";
+        resout<<a<<"\n";
+        for(int i=0;i<c*a;i++) resout<<bias[i]<<"\n";
+    }
+    else{
+        std::cout<<"Invalid operation"<<std::endl;
+        return 1;
+    }
+    return 0;
 }
--- Task1/Task1.2/matrix_pthread.cpp
+++ Task1/Task1.2/matrix_pthread.cpp
@@ -12,7 +12,7 @@ void* thrfunc(void* arg){
         int i = r++;
         while(i < da){
             for (int j = 0; j < dc; j++){
-                cout << i<<","<<j << endl;
+                // cout << i<<","<<j << endl;
                 for (int k = 0; k < db; k++){
                     Bp[i][j] += Ip[i][k] * Wp[k][j];
                 }
@@ -42,7 +42,7 @@ void fullyConnectedPthread(float* inp,float* weight,float* bias,int a,int b,int 
             Bp[i][j] = bias[a*j + i];
         }
     }
-    th = 2;
+    th = 4;
     pthread_t threads[th];
     // pthread_attr_t attr;
     // pthread_attr_init(&attr);
@@ -58,4 +58,52 @@ void fullyConnectedPthread(float* inp,float* weight,float* bias,int a,int b,int 
             bias[a*j + i] = Bp[i][j];
         }
     }
+}
+
+int main(int argc,char** argv){
+    std::string* args=new std::string[argc];
+    for(int i=0;i<argc;i++){
+        std::string temp(argv[i]);
+        args[i]=temp;
+    }
+    if(argc!=7){
+        std::cout<<"Invalid number of arguments"<<std::endl;
+        return 1;
+    }
+    if(args[1]!="fullyconnected"){
+        std::cout<<"Invalid operation to be performed"<<std::endl;
+        return 1;
+    }
+    std::ifstream inpin(args[2]);
+    std::ifstream weightin(args[3]);
+    std::ifstream biasin(args[4]);
+    std::ofstream resout(args[5]);
+    if(!inpin || !weightin || !biasin){
+        std::cout<<"File not found"<<std::endl;
+        return 1;
+    }
+    if(args[6]=="pthread"){
+        int b,a,c;
+        inpin>>b>>a;
+        float* inp=new float[a*b];
+        for(int i=0;i<a*b;i++) inpin>>inp[i];
+        weightin>>c>>b;
+        float* weight=new float[b*c];
+        for(int i=0;i<b*c;i++) weightin>>weight[i];
+        biasin>>c>>a;
+        float* bias=new float[c*a];
+        for(int i=0;i<c*a;i++) biasin>>bias[i];
+        auto start=std::chrono::high_resolution_clock::now();
+        fullyConnectedPthread(inp,weight,bias,a,b,c);
+        auto end=std::chrono::high_resolution_clock::now();
+        std::cout<<std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()<<std::endl;
+        resout<<c<<"\n";
+        resout<<a<<"\n";
+        for(int i=0;i<c*a;i++) resout<<bias[i]<<"\n";
+    }
+    else{
+        std::cout<<"Invalid operation"<<std::endl;
+        return 1;
+    }
+    return 0;
 }
