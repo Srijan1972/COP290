ceafa450a054e9367680d9c352a36e42cc12c57d
NischayDiwan <cs5200433@iitd.ac.in>
2022-02-20 15:51:48 +0530

    p thread modified, with th is variable number of threads

--- Task1/Task1.2/matrix_pthread.cpp
+++ Task1/Task1.2/matrix_pthread.cpp
@@ -2,17 +2,24 @@
 using namespace std;
 
 int da,db,dc;
-float Ip[1000][1000];
-float Wp[1000][1000];
-float Bp[1000][1000];
+float Ip[1000][1000]={0};
+float Wp[1000][1000]={0};
+float Bp[1000][1000]={0};
 int r = 0;
-
-void* thrfunc(void* arg)
-{
-    int i = r++;
-    for (int j = 0; j < dc; j++)
-      for (int k = 0; k < db; k++)
-        Bp[i][j] += Ip[i][k] * Wp[k][j];
+int th;
+void* thrfunc(void* arg){
+    if(r < da){
+        int i = r++;
+        while(i < da){
+            for (int j = 0; j < dc; j++){
+                cout << i<<","<<j << endl;
+                for (int k = 0; k < db; k++){
+                    Bp[i][j] += Ip[i][k] * Wp[k][j];
+                }
+            }
+            i = i+th;
+        }
+    }
 }
 void fullyConnectedPthread(float* inp,float* weight,float* bias,int a,int b,int c){
 	da = a;
@@ -35,15 +42,15 @@ void fullyConnectedPthread(float* inp,float* weight,float* bias,int a,int b,int 
             Bp[i][j] = bias[a*j + i];
         }
     }
-
-    pthread_t threads[a];
+    th = 2;
+    pthread_t threads[th];
     // pthread_attr_t attr;
     // pthread_attr_init(&attr);
-    for (int i = 0; i < a; i++) {
-        int* p;
-        pthread_create(&threads[i], NULL, thrfunc, (void*)(p));
+    for (int i = 0; i < th; i++) {
+        int* k;
+        pthread_create(&threads[i], NULL, thrfunc, (void*)(k));
     }
-    for (int i = 0; i < a; i++) {
+    for (int i = 0; i < th; i++) {
         pthread_join(threads[i], NULL);  
     }
     for (int i = 0; i < a; i++) {
